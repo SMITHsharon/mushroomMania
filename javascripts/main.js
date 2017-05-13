fe8c891acc566bba8f9cfd10c12648c975038981
sharon smith <sharon.e.smith2@gmail.com>
2017-05-13 14:48:01 -0500

    setting up showMushroomList

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,33 @@
-console.log("writing to console");
+app.run((FIREBASE_CONFIG) => {
+	firebase.initializeApp(config);
+});
+
+
+app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+
+	$scope.showMushroomList = true;
+	$scope.items = [];
+
+
+	let getMushroomList = () => {
+
+		let itemz = [];
+
+		return $q((resolve, reject) => {
+			$http.get(`$FIREBASE_CONFIG.databaseURL}/items.json`)
+			.then((fbItems) => {
+				var itemCollection = fbItems.data;
+				Object.keys(itemCollection).forEach((key) => {
+					itemCollection[key]id=key;
+					itemz.push(itemCollection[key]);
+				});
+			});
+			resolve(itemz);
+		});
+		.catch((error) => {
+			reject(error);
+		})
+	};
+
+
+});
