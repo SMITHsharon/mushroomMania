d3468b6f179915638e4acb0e6a5f41d4bd9071b1
sharon smith <sharon.e.smith2@gmail.com>
2017-05-13 22:53:33 -0500

    writing unfiltered database to DOM

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,5 @@
 app.run((FIREBASE_CONFIG) => {
-	firebase.initializeApp(config);
+	firebase.initializeApp(FIREBASE_CONFIG);
 });
 
 
@@ -14,20 +14,33 @@ app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
 		let itemz = [];
 
 		return $q((resolve, reject) => {
-			$http.get(`$FIREBASE_CONFIG.databaseURL}/items.json`)
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/mushrooms.json`)
 			.then((fbItems) => {
-				var itemCollection = fbItems.data;
+				let itemCollection = fbItems.data;
+console.log("fbItems.data :: ", fbItems.data);
 				Object.keys(itemCollection).forEach((key) => {
-					itemCollection[key]id=key;
+					itemCollection[key].id = key;
 					itemz.push(itemCollection[key]);
 				});
+					resolve(itemz);
+console.log("itemz :: ", itemz);
+			})
+			.catch((error) => {
+				reject(error);
 			});
-			resolve(itemz);
 		});
-		.catch((error) => {
-			reject(error);
-		})
 	};
+
+	let getMushrooms = () => {
+
+		getMushroomList().then((itemz) => {
+			$scope.mushrooms = itemz;
+		}).catch((error) => {
+			console.log("get Error", error);
+		});
+	};
+
+	getMushrooms();
 
 
 });
